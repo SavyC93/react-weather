10e916b020fa926a9da6f40245a205f30ba8b2ae
SavyC93 <SavyC93@gmail.com>
2023-07-27 19:02:03 -0600

    cleaned up html and css, then added axios to index

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Humidity from "./Humidity";
 import Wind from "./Wind";
+import "./Temperature.css";
 
 export default function Temperature() {
   return (
-    <div>
-      <h1> 30°C </h1>
-      <Humidity />
-      <Wind />
+    <div className="row">
+      <div className="col-6">
+        <div className="temperature">
+          <strong>30</strong>
+          <span className="unit">°C</span>
+        </div>
+        <p>Clear Sky</p>
+      </div>
+      <div className="col-6">
+        <Humidity />
+        <Wind />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,31 +2,38 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import React from "react";
 import Search from "./Search";
+import axios from "axios";
 import Temperature from "./Temperature";
 import Forecast from "./Forecast";
 import "./App.css";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <Search />
-      <h1>Hey Beautiful! Let's see what it's like in...</h1>
-      <h2>Denver</h2>
-      <p>Last updated on Monday at 12:10pm</p>
-      <p>Clear Sky</p>
-      <Temperature />
-      <Forecast />
-      <p>
-        <a
-          href="https://github.com/SavyC93/react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-sourced
-        </a>{" "}
-        by Savannah Chin
-      </p>
+      <div className="container">
+        <div className="weather-app">
+          <Search />
+          <h1>Hey Beautiful! How is it in...</h1>
+          <h1>
+            <strong>Denver</strong>
+          </h1>
+          <p>Last updated on Monday at 12:10pm</p>
+          <Temperature />
+          <Forecast />
+        </div>
+        <p>
+          <a
+            href="https://github.com/SavyC93/react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced
+          </a>{" "}
+          by Savannah Chin
+        </p>
+      </div>
     </div>
   );
 }
